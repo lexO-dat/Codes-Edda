f1a31340d3d54c95a0271a64b96e92e81dd64268
Lucas Abello <124079018+lexO-dat@users.noreply.github.com>
2023-08-21 00:01:56 -0400

    Delete tarea1_ejercicio3.java

--- tarea1_ejercicio3.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-import java.io.*;
-
-public class tarea1_ejercicio3 {
-    public static void main(String[] args){
-        Scanner a = new Scanner(System.in);
-        Integer p = a.nextBigInteger(radix);
-    }
-}
